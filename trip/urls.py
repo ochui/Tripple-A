b6185f109993c3706660c5ab4c566ae7446f8803
ochui princewill patrick <ochuiprincewill411@gmail.com>
2018-11-24 13:31:29 +0100

    moved template to template/trip

--- trip/urls.py
+++ trip/urls.py
@@ -8,5 +8,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('pages.urls'))
+    path("", include('pages.urls')),
+    path("accounts/", include('allauth.urls'))
 ]
