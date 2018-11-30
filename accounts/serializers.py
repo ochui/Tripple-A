45c237d85d4da74ee6f8b3454cfcd64c4f260094
ochui princewill patrick <ochuiprincewill411@gmail.com>
2018-11-30 11:42:08 +0100

    added extra fields to CustomRegisterSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,8 +9,16 @@ from rest_auth.registration.serializers import RegisterSerializer
 class CustomRegisterSerializer(RegisterSerializer):
     
     phone_number = serializers.CharField(required=True)
+    gender = serializers.CharField(required=False)
+    date_of_birth = serializers.CharField(required=False)
+    state = serializers.CharField(required=False)
+    city = serializers.CharField(required=False)
 
     def custom_signup(self, request, user):
         
         user.phone_number = self.validated_data.get('phone_number')
-        user.save(update_fields=['phone_number'])
+        user.genger = self.validated_data.get('gender')
+        user.date_of_birth = self.validated_data.get('date_of_birth')
+        user.state = self.validated_data.get('state')
+        user.city = self.validated_data.get('city')
+        user.save(update_fields=['phone_number', 'gender', 'date_of_birth', 'state', 'city'])
