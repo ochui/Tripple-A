f6c78291065fa501374e85ac12d9d1f575d00aa8
ochui princewill patrick <ochuiprincewill411@gmail.com>
2018-12-28 16:55:49 +0100

    fix url path

--- trip/urls.py
+++ trip/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("", include('pages.urls')),
     path("accounts/", include('allauth.urls')),
     path('api/v1/', include('cab.urls')),
-    path('api/v1/auth', include('djoser.urls')), 
-    path('api/v1/auth', include('djoser.urls.authtoken')),
+    path('api/v1/auth/', include('djoser.urls')), 
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
 ]
